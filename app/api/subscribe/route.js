8e83b05e2578b0384d4d3b9d58ea7f414fd65586
sakshamwithweb <websaksham39@gmail.com>
2025-02-09 16:57:49 +0530

    checng extistance first

--- app/api/subscribe/route.js
+++ app/api/subscribe/route.js
@@ -4,12 +4,14 @@ import { NextResponse } from "next/server"
 export async function POST(req) {
     try {
         const { emailId } = await req.json()
+        const checkExist = await SubscribeEmail.findOne({ email: emailId })
+        if (checkExist) return NextResponse.json({ success: false })
         const newDoc = new SubscribeEmail({
-            email:emailId
+            email: emailId
         })
         await newDoc.save()
         return NextResponse.json({ success: true })
     } catch (error) {
-        return NextResponse.json({success:false})
+        return NextResponse.json({ success: false })
     }
 }
