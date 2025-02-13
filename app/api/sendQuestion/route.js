6f126963bf4491bf88b6880f09ddffb9393a52b7
sakshamwithweb <websaksham39@gmail.com>
2025-02-13 20:27:33 +0530

    verified that all inputs are required in both frontend and backend

--- app/api/sendQuestion/route.js
+++ app/api/sendQuestion/route.js
@@ -2,10 +2,10 @@ import { Question } from "@/lib/models/question";
 import connectDb from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
-
 export async function POST(params) {
     try {
         const { question } = await params.json();
+        if (!question || question?.trim()?.length == 0) throw new Error("Enter valid question");
         await connectDb()
         const newQ = new Question({
             question: question
--- app/api/subscribe/route.js
+++ app/api/subscribe/route.js
@@ -1,9 +1,11 @@
 import { SubscribeEmail } from "@/lib/models/subscribeEmail"
 import { NextResponse } from "next/server"
+import isEmail from 'validator/lib/isEmail';
 
 export async function POST(req) {
     try {
         const { emailId } = await req.json()
+        if (!emailId || emailId?.trim()?.length == 0 || !isEmail(emailId)) throw new Error("Enter valid email");
         const checkExist = await SubscribeEmail.findOne({ email: emailId })
         if (checkExist) return NextResponse.json({ success: false })
         const newDoc = new SubscribeEmail({
