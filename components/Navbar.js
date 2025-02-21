8e3aa1f56d9caaaad639989335bfe66cb1e7b58c
sakshamwithweb <websaksham39@gmail.com>
2025-02-21 11:56:40 +0530

    add a top loading like line

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,10 +12,37 @@ import {
 } from "@/components/ui/sheet"
 import { ModeToggle } from './ui/mode'
 import { Tooltip as ReactTooltip } from 'react-tooltip'
-
+import LoadingBar from 'react-top-loading-bar';
+import { usePathname } from 'next/navigation';
 const Navbar = () => {
+  const [progress, setProgress] = useState(0)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setProgress(20)
+
+    setTimeout(() => {
+      setProgress(40)
+    }, 100);
+
+    setTimeout(() => {
+      setProgress(100)
+    }, 400);
+  }, [pathname])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setProgress(0)
+    }, 500);
+  }, [])
+  
   return (
     <nav className='flex mx-auto sticky top-0 p-2 md:p-2 justify-between items-center backdrop-blur border border-b-gray-400/50 z-50'>
+      <LoadingBar
+        color="#9333EA"
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <div className='font-bold text-xl mx-2 md:mx-4'>
         <Link href={"http://webwithsaksham.com"}>SakshamWithWeb</Link>
       </div>
